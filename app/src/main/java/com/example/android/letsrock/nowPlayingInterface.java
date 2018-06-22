89bf769c46f0fcc5ed1240f060345abafaa1c7a3
falcomcpherson <falco.mcpherson@gmail.com>
2018-06-22 09:08:34 -0400

    Project completed

---
+++ app/src/main/java/com/example/android/letsrock/nowPlayingInterface.java
@@ -0,0 +1,28 @@
+package com.example.android.letsrock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class nowPlayingInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_now_playing );
+        updatePlayingInfo ();
+    }
+
+    public void updatePlayingInfo() {
+        Intent nowPlayingIntent = getIntent();
+        Bundle extras = nowPlayingIntent.getExtras();
+        String playingArtist = extras.getString ("playingArtist");
+        String playingSong = extras.getString ( "playingSong");
+        TextView nowPlayingSongView = (TextView) findViewById ( R.id.nowPlayingSong );
+        nowPlayingSongView.setText (playingSong);
+        TextView nowPlayingArtistView = (TextView) findViewById ( R.id.nowPlayingArtist );
+        nowPlayingArtistView.setText ( playingArtist );
+    }
+
+}
